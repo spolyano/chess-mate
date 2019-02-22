193eb95b039a82a145cf2da4a2874ca513047fe5
doma4k <sergey.polyanovsky@yahoo.de>
2019-02-22 14:59:14 +0000

    ready path

--- ChessBoard.java
+++ ChessBoard.java
@@ -3,84 +3,58 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class ChessBoard {
-	private ArrayList<ChessFigure> figures;
 	Scanner keyRead;
-	
-	private static int[][] boardCells = new int[8][8];
+	private static ChessFigure[][] figureCells = new ChessFigure[8][8];
 	
 	// **internal methods **
 
 	private int[] inputPosition() {
 		int[] input = new int[2];
-		System.out.print("x [0-7] ");
+		System.out.print("i [0-7] ");
 		input[0] = keyRead.nextInt();
-		System.out.print("y [0-7] ");
+		System.out.print("j [0-7] ");
 		input[1] = keyRead.nextInt();
 		return input;
 	}
 	
-	private void changePosition(int figureID, int[] posCurrentXY, int[] posNewXY) {
-		boardCells[posCurrentXY[0]][posCurrentXY[1]] = -1;
-		boardCells[posNewXY[0]][posNewXY[1]] = figureID;
-		figures.get(figureID).setPosition(posNewXY[0], posNewXY[1]);
+	private void changePosition(int[] posCurrentXY, int[] posNewXY) {
+		figureCells[posNewXY[0]][posNewXY[1]] = figureCells[posCurrentXY[0]][posCurrentXY[1]];
+		figureCells[posNewXY[0]][posNewXY[1]].setPosition(posNewXY[0], posNewXY[1]);
+		figureCells[posCurrentXY[0]][posCurrentXY[1]] = null;
 	}
 
 	// ** static methods **
 	
-	public static int[][] getBoardCells(){
-		return boardCells;
+	public static ChessFigure[][] getFigureCells(){
+		return figureCells;
 	}
 	
 	// ** public methods **
 	
 	public void init() {
-		figures = new ArrayList<ChessFigure>();
-		
-		// -1 == no figure
-		for (int i = 0; i< 8; i++)
-			for (int j = 0; j< 8; j++)
-				boardCells[i][j] = -1;
-		
-		int figureID = 0;	
 		//create 8 pawns
-		for (int i = 0; i < 8; i++) {
-			figures.add(figureID, new Pawn(figureID, ChessFigure.WHITE, i, 1));
-			boardCells[i][1] = figureID;
-			figureID++;
-			
-			figures.add(figureID, new Pawn(figureID, ChessFigure.BLACK, i, 6));
-			boardCells[i][6] = figureID;
-			figureID++;
+		for (int j = 0; j < 8; j++) {	
+			figureCells[1][j] = new Pawn(ChessFigure.ENEMY, 1, j);
+			figureCells[6][j] = new Pawn(ChessFigure.YOU, 6, j);
 		}
-		
 		//create 2 bishops
-		for (int i = 2; i < 6; i+=3) {
-			figures.add(figureID, new Bishop(figureID, ChessFigure.WHITE, i, 0));
-			boardCells[i][0] = figureID;
-			figureID++;
-			
-			figures.add(figureID, new Bishop(figureID, ChessFigure.BLACK, i, 7));
-			boardCells[i][7] = figureID;
-			figureID++;
-		}
-		
+		for (int j = 2; j < 6; j+=3) {		
+			figureCells[0][j] = new Bishop(ChessFigure.ENEMY, 0, j);
+			figureCells[7][j] = new Bishop(ChessFigure.YOU, 7, j);
+		}	
 	}
 	
 	public void drawBoard() {
+		System.out.print("\n[---]");
+		for (int j = 0; j < 8; j++)
+			System.out.print("[j:"+ j +"]");
+		System.out.print("\n\n");
 		for (int i = 0; i < 8; i++) {
-			if (i == 0) {
-				System.out.print("[---]");
-				for (int j = 0; j < 8; j++)
-					System.out.print("[x:"+ j +"]");
-				System.out.print("\n\n");
-			}
-			System.out.print("[y:"+ i +"]");
-			for (int j = 0; j < 8; j++) {
-				int fPos = boardCells[j][i];
-				if (fPos >= 0)
-					figures.get(fPos).printFigure();
+			System.out.print("[i:"+ i +"]");
+			for (int j = 0; j < 8; j++)
+				if(figureCells[i][j] != null)
+					figureCells[i][j].printFigure();
 				else System.out.print("[ x ]");
-			}
 			System.out.print("\n");
 		}
 		System.out.print("\n");
@@ -92,7 +66,7 @@ public class ChessBoard {
 		
 		int[] posCurrentXY = new int[2];
 		int[] posNewXY = new int[2];
-		int[][] posList = new int[64][2];
+		ArrayList<int[]> posArray;
 		
 		//test for - 4-mal
 		//any key but number will break
@@ -101,26 +75,23 @@ public class ChessBoard {
 		System.out.println("Select figure: ");
 		posCurrentXY = inputPosition();
 		
-		/*viable solution
-		 * figureID = boardCells[posCurrentXY[0]][posCurrentXY[1]];
-		 * posList = figures.get(figureID).getPositionList();
-		 * changePosition(figureID, posCurrentXY, posNewXY);
-		*/
-		
-		int figureID = boardCells[posCurrentXY[0]][posCurrentXY[1]];
-		if (figureID >= 0) {
-			//print possible positions
-			posList = figures.get(figureID).getPositionList();
-			System.out.println(Arrays.deepToString(posList));
+		if (figureCells[posCurrentXY[0]][posCurrentXY[1]] != null) {
 			
-			if (posList[0][0] > 0) {
+			posArray = figureCells[posCurrentXY[0]][posCurrentXY[1]].getPositionList();
+			System.out.println(Arrays.deepToString(posArray.toArray()));
+			
+			if (posArray.size() > 0){
 				//set to new position
 				System.out.println("Select new position: ");
 				posNewXY = inputPosition();
 				
 				//test set
-				changePosition(figureID, posCurrentXY, posNewXY);
-				drawBoard();
+				for (int pos = 0; pos < posArray.size(); pos++) 
+				    if (Arrays.equals(posArray.get(pos), posNewXY)) {
+				    	changePosition(posCurrentXY, posNewXY);
+						drawBoard();
+						break;
+				    }
 			}
 		}
 		
--- ChessFigure.java
+++ ChessFigure.java
@@ -1,30 +1,29 @@
+import java.util.ArrayList;
 
 public abstract class ChessFigure {
 	
-	public final static int WHITE = 0;
-	public final static int BLACK = 1;
+	public final static int ENEMY = 0;// ENEMY
+	public final static int YOU = 1;// YOU
 	
 	public final static int PAWN = 1;
 	public final static int ROOK = 2;
 	public final static int BISHOP = 3;
 	
-	int pos_x;
-	int pos_y;
+	int pos_j;
+	int pos_i;
 	int rank;
 	int color;
-	int figureID;//not sure if needed
 	
-	public ChessFigure(int figureID, int rank, int color, int pos_x, int pos_y) {
-		this.pos_x = pos_x;
-		this.pos_y = pos_y;
+	public ChessFigure(int rank, int color, int pos_i, int pos_j) {
+		this.pos_j = pos_j;
+		this.pos_i = pos_i;
 		this.rank = rank;
 		this.color = color;
-		this.figureID = figureID;
 	}
 	
-	public void setPosition(int pos_x, int pos_y) {
-		this.pos_x = pos_x;
-		this.pos_y = pos_y;
+	public void setPosition(int pos_i, int pos_j) {
+		this.pos_j = pos_j;
+		this.pos_i = pos_i;
 	}
 	
 	public int getColor() {
@@ -39,106 +38,77 @@ public abstract class ChessFigure {
 		System.out.print("["+ rank + "-" + color + "]");	
 	}
 		
-	public abstract int[][] getPositionList();
+	public abstract ArrayList<int[]> getPositionList();
 }
 
 final class Pawn extends ChessFigure{
 	
-	public Pawn(int figureID, int color, int pos_x, int pos_y) {
-		super(figureID, PAWN, color, pos_x, pos_y);
+	public Pawn(int color, int pos_i, int pos_j) {
+		super(PAWN, color, pos_i, pos_j);
 	}
 
-	public int[][] getPositionList(){
-		int[][] posList = new int[64][2];
-		int[][] cells = ChessBoard.getBoardCells();
+	public ArrayList<int[]> getPositionList(){
+		ArrayList<int[]> posArray = new ArrayList<int[]>();
+		ChessFigure[][] figures = ChessBoard.getFigureCells();
 		
-		if (color == WHITE) {
-			if (pos_y == 1) {
-				if ((cells[pos_x][pos_y + 1] < 0)&&(cells[pos_x][pos_y + 2] < 0)) {	
-					posList[0][0] = pos_x;
-					posList[0][1] = pos_y + 1;
-					posList[1][0] = pos_x;
-					posList[1][1] = pos_y + 2;
+		if (color == ENEMY) {
+			if (pos_i == 1) {
+				if ((figures[pos_i + 1][pos_j] == null)&&(figures[pos_i + 2][pos_j] == null)) {	
+					posArray.add(new int[] {pos_i + 1, pos_j});
+					posArray.add(new int[] {pos_i + 2, pos_j});
 				}
-				else if ((cells[pos_x][pos_y + 1] < 0)&&(cells[pos_x][pos_y + 2] >= 0)) {
-					posList[0][0] = pos_x;
-					posList[0][1] = pos_y + 1;
-				}
+				else if ((figures[pos_i + 1][pos_i] == null)&&(figures[pos_i + 2][pos_j] != null))
+					posArray.add(new int[] {pos_i + 1, pos_j});
 			}
-			else if (pos_y < 7) {
-				if (cells[pos_x][pos_y + 1] < 0) {
-					posList[0][0] = pos_x;
-					posList[0][1] = pos_y + 1;
-				}
+			else if (pos_i < 7) {
+				if (figures[pos_i + 1][pos_j] == null)
+					posArray.add(new int[] {pos_i + 1, pos_j});
 			}	
 		}
-		else if (color == BLACK) {
-			if (pos_y == 6) {
-				if ((cells[pos_x][pos_y - 1] < 0)&&(cells[pos_x][pos_y - 2] < 0)) {	
-					posList[0][0] = pos_x;
-					posList[0][1] = pos_y - 1;
-					posList[1][0] = pos_x;
-					posList[1][1] = pos_y - 2;
+		else if (color == YOU) {
+			if (pos_i == 6) {
+				if ((figures[pos_i - 1][pos_j] == null)&&(figures[pos_i - 2][pos_j] == null)) {	
+					posArray.add(new int[] {pos_i - 1, pos_j});
+					posArray.add(new int[] {pos_i - 2, pos_j});
 				}
-				else if ((cells[pos_x][pos_y - 1] < 0)&&(cells[pos_x][pos_y - 2] >= 0)) {
-					posList[0][0] = pos_x;
-					posList[0][1] = pos_y - 1;
-				}
+				else if ((figures[pos_i - 1][pos_j] == null)&&(figures[pos_i - 2][pos_j] != null))
+					posArray.add(new int[] {pos_i - 1, pos_j});
 			}
-			else if (pos_y > 0)  {
-				if (cells[pos_x][pos_y - 1] < 0) {
-					posList[0][0] = pos_x;
-					posList[0][1] = pos_y - 1;
-				}
+			else if (pos_i > 0)  {
+				if (figures[pos_i - 1][pos_j] == null)
+					posArray.add(new int[] {pos_i - 1, pos_j});
 			}	
 		}	
-		return posList;
+		return posArray;
 	}	
 	
 }//Pawn
 
 final class Bishop extends ChessFigure{
 	
-	public Bishop(int figureID, int color, int pos_x, int pos_y) {
-		super(figureID, BISHOP, color, pos_x, pos_y);
+	public Bishop(int color, int pos_i, int pos_j) {
+		super(BISHOP, color, pos_i, pos_j);
 	}
 
-	public int[][] getPositionList(){
-		int[][] posList = new int[64][2];
-		int[][] cells = ChessBoard.getBoardCells();
+	public ArrayList<int[]> getPositionList(){
+		ArrayList<int[]> posArray = new ArrayList<int[]>();
+		ChessFigure[][] figures = ChessBoard.getFigureCells();
 		
 		boolean canMove = true;
 		int i;
-		int amount = 0;
 		
-		//N-W
+		//N-E
 		i = 1;
 		canMove = true;
 		while (canMove) {
-			if ((pos_x + i <= 7)&&(pos_y - i >= 0))
-				if ((cells[pos_x + i][pos_y - i] < 0)) {
-					posList[amount][0] = pos_x + i;
-					posList[amount][1] = pos_y - i;	
+			if ((pos_j + i <= 7)&&(pos_i - i >= 0))
+				if ((figures[pos_i - i][pos_j + i] == null)) {	
+					posArray.add(new int[] {pos_i - i, pos_j + i});				
 					i++;
-					amount++;
 				}
 				else {
-					canMove = false;
-				}
-			else canMove = false;
-		}
-		//S-W
-		i = 1;
-		canMove = true;
-		while (canMove) {
-			if ((pos_x + i <= 7)&&(pos_y + i <= 7))
-				if ((cells[pos_x + i][pos_y + i] < 0)) {
-					posList[amount][0] = pos_x + i;
-					posList[amount][1] = pos_y + i;	
-					i++;
-					amount++;
-				}
-				else {
+					if(figures[pos_i - i][pos_j + i].getColor() != color)
+						posArray.add(new int[] {pos_i - i, pos_j + i});
 					canMove = false;
 				}
 			else canMove = false;
@@ -147,35 +117,51 @@ final class Bishop extends ChessFigure{
 		i = 1;
 		canMove = true;
 		while (canMove) {
-			if((pos_x - i >= 0)&&(pos_y + i <= 7))
-				if ((cells[pos_x - i][pos_y + i] < 0)) {
-					posList[amount][0] = pos_x - i;
-					posList[amount][1] = pos_y + i;	
+			if ((pos_j + i <= 7)&&(pos_i + i <= 7))
+				if ((figures[pos_i + i][pos_j + i] == null)) {
+					posArray.add(new int[] {pos_i + i, pos_j + i});				
 					i++;
-					amount++;
 				}
 				else {
+					if(figures[pos_i + i][pos_j + i].getColor() != color)
+						posArray.add(new int[] {pos_i + i, pos_j + i});
 					canMove = false;
 				}
 			else canMove = false;
 		}
-		//N-E
+		//S-W
 		i = 1;
 		canMove = true;
 		while (canMove) {
-			if((pos_x - i >= 0)&&(pos_y - i >= 0))
-				if ((cells[pos_x - i][pos_y - i] < 0)) {
-					posList[amount][0] = pos_x - i;
-					posList[amount][1] = pos_y - i;	
+			if((pos_j - i >= 0)&&(pos_i + i <= 7))
+				if ((figures[pos_i + i][pos_j - i] == null)) {			
+					posArray.add(new int[] {pos_i + i, pos_j - i});				
 					i++;
-					amount++;
 				}
 				else {
+					if(figures[pos_i + i][pos_j - i].getColor() != color)
+						posArray.add(new int[] {pos_i + i, pos_j - i});
 					canMove = false;
 				}
 			else canMove = false;
 		}
-		return posList;
+		//N-W
+		i = 1;
+		canMove = true;
+		while (canMove) {
+			if((pos_j - i >= 0)&&(pos_i - i >= 0))
+				if ((figures[pos_i - i][pos_j - i] == null)) {
+					posArray.add(new int[] {pos_i - i, pos_j - i});				
+					i++;;
+				}
+				else {
+					if(figures[pos_i - i][pos_j - i].getColor() != color)
+						posArray.add(new int[] {pos_i - i, pos_j - i});
+					canMove = false;
+				}
+			else canMove = false;
+		}
+		return posArray;
 	}	
 	
 }//Bishop
